6137d7b8885cacfd95c383e1aba3c137f2ab776f
SegoUduk <ajikiar4@gmail.com>
2026-06-29 13:43:16 +0700

    fixxxxxxx

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 // File ini cuma bertugas memanggil Express dari folder backend
-const app = require('../backend/index.js'); // Sesuaikan 'index.js' dengan nama file utama backend-mu
+const app = require('../backend/server.js'); // Sesuaikan 'index.js' dengan nama file utama backend-mu
 
 module.exports = app;
